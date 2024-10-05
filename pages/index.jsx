9c9ec0e825d57cf1db5d00d0d87327965209b3b7
tevograce <danil-krotter@mail.ru>
2024-10-05 14:04:31 +0500

    current status 20less

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+// import { Game } from "../components/game/game";
+
+// export default function HomePage() {
+//   return <Game />;
+// }
+
+export default function HomePage() {
+  return (
+<div className="bg-slate-50 min-h-screen"> 
+
+</div>
+
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        "game-row": "repeat(3, 30px)",
+      },
+    },
+  },
+  plugins: [],
+};
