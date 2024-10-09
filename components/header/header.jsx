897c2d5288958e733a16ddaa37b2b144d1362f51
tevograce <danil-krotter@mail.ru>
2024-10-09 20:32:48 +0500

    lesson 22 finish

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 import logoSrc from "./logo.svg";
 import { Profile } from "../profile";
 import { ArrowDownIcon } from "./arrow-down-icon";
+import { UiButton } from "../uikit/ui-button";
 
 export function Header() {
   return (
     <header className="flex h-24 items-center px-8 bg-white shadow-lg">
       <Image src={logoSrc} alt="logo" />
       <div className="w-px h-8 bg-slate-200 mx-6"></div>
-      <button className="w-44 bg-teal-600 hover:bg-teal-500 transition-colors text-white rounded-lg h-11 px-5 py-2 text-2xl leading-tight ">
-        Играть
-      </button>
-
+      <UiButton className="w-44" variant="primary" size="lg" >Играть</UiButton>
+    
       <button className="ml-auto flex items-center gap-2 text-start text-teal-600">
         <Profile/>
         <ArrowDownIcon/>
@@ -20,3 +19,4 @@ export function Header() {
     </header>
   );
 }
+ 
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import "../styles/global.css";
 
 import { Inter } from 'next/font/google'
@@ -6,7 +7,7 @@ const inter = Inter({ subsets: ['latin', 'cyrillic'], display: 'swap', })
 
 export default function App({ Component, getProps }) {
   return  (
-  <div className={inter.className}>
+  <div className={clsx(inter.className, 'text-slate-900')}>
     <Component {...getProps} /> 
   </div>
   )
